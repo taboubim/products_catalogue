4b88ae0aca2e9d2d016191203840a5d3abe6a47c
Mohamed Taboubi <taboubim@hotmail.com>
2016-07-14 13:38:08 -0400

    Adding main ajax calls

---
+++ Front_module/src/main/webapp/common_libs/XHRequests.js
@@ -0,0 +1,55 @@
+var ajaxReq = function () {
+
+    var api = {};
+
+    api.load = function (url, success, failure) {
+        return $.ajax({
+            type: 'GET',
+            url: url,
+            cache: false
+        }).done(success).error(failure);
+    };
+
+    api.create = function (object, url, success, failure) {
+        return $.ajax({
+            type: 'POST',
+            data: $.toJSON(object),
+            url: url
+        }).done(success).error(failure);
+    };
+
+    api.update = function (object, url, success, failure) {
+        return $.ajax({
+            type: 'PUT',
+            data: $.toJSON(object),
+            url: url,
+            success: success,
+            error: failure
+        });
+    };
+
+    api.put = function (object, url, success, failure) {
+        return $.ajax({
+            headers: api.commonHeaders(),
+            type: 'PUT',
+            data: $.toJSON(object),
+            url: url
+        }).done(success).error(failure);
+    };
+
+    api.remove = function (object, url, success, failure) {
+        return $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: success,
+            error: failure
+        });
+    };
+
+    return api;
+
+};
+
+
+
+
