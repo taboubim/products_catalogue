7dec615bd530c311ba31e4c3cc55db7483cbe29e
Mohamed Taboubi <taboubim@hotmail.com>
2016-07-18 12:41:03 -0400

    Adding the main modules to the Guice Injector

--- Rest_module/src/main/java/ca/products/services/InjectorListener.java
+++ Rest_module/src/main/java/ca/products/services/InjectorListener.java
@@ -4,12 +4,17 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
+
 /**
  * Created by mtaboubi on 16-07-17.
  */
 public class InjectorListener extends GuiceServletContextListener {
     @Override
     protected Injector getInjector() {
-        return Guice.createInjector(new RestModule());
+
+        return Guice.createInjector(
+                new PersistenceModule(),
+                new GuiceModule(),
+                new RestModule());
     }
 }
