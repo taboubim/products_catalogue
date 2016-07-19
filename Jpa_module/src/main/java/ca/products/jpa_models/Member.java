adf8dc25863cc0390a127a99ae591db2143149c9
Mohamed Taboubi <taboubim@hotmail.com>
2016-07-19 10:20:23 -0400

    re-add Member

---
+++ Jpa_module/src/main/java/ca/products/jpa_models/Member.java
@@ -0,0 +1,51 @@
+package ca.products.jpa_models;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created by mtaboubi on 16-07-15.
+ */
+@Entity
+@Table(name = "shopping_member")
+public class Member {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+
+    private String pseudonym;
+
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "member")
+    private ShoppingBag shoppingBag;
+
+    public Member(String pseudonym) {
+        this.pseudonym = pseudonym;
+    }
+
+    public Member() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPseudonym() {
+        return pseudonym;
+    }
+
+    public ShoppingBag getShoppingBag() {
+        return shoppingBag;
+    }
+
+    public void setShoppingBag(ShoppingBag shoppingBag) {
+        this.shoppingBag = shoppingBag;
+    }
+}
