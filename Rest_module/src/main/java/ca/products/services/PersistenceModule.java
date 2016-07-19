5c6aa8f6756a1824865b92bf392562ce530d55a7
Mohamed Taboubi <taboubim@hotmail.com>
2016-07-19 01:55:12 -0400

    Refactoring : duplication - persistence properties are declared already in persistence.xml

--- Rest_module/src/main/java/ca/products/services/PersistenceModule.java
+++ Rest_module/src/main/java/ca/products/services/PersistenceModule.java
@@ -4,6 +4,7 @@ package ca.products.services;
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import com.google.inject.persist.jpa.JpaPersistModule;
+
 import java.util.Properties;
 
 /**
@@ -12,23 +13,7 @@ import java.util.Properties;
 public class PersistenceModule implements Module {
     @Override
     public void configure(Binder binder) {
-        binder
-                .install(new JpaPersistModule("manager1")
-                        .properties(getPersistenceProperties()));
-
+        binder.install(new JpaPersistModule("manager1"));
         binder.bind(PersistenceInitializer.class).asEagerSingleton();
-    }
-
-    private static Properties getPersistenceProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        properties.put("hibernate.connection.url", "jdbc:postgresql://localhost:5432/postgres");
-        properties.put("hibernate.connection.username", "postgres");
-        properties.put("hibernate.connection.password", "postgres");
-        properties.put("hibernate.connection.pool_size", "1");
-        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        properties.put("hibernate.hbm2ddl.auto", "create");
-
-        return properties;
     }
 }
